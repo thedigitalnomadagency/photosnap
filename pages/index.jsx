70e8b736ccca224fb50dd7e0fca299a8f9bf7ff8
thedigitalnomadagency <delasimensa@gmail.com>
2021-02-19 18:48:01 +0000

    updates

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 const Home = () => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Home Page of photosnap website</h1>
     </Layout>
   );
 };
