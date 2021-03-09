d1cdb110532327eba7467041d63717b3d52fd907
thedigitalnomadagency <delasimensa@gmail.com>
2021-03-09 12:03:15 +0000

    updates

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,13 +8,11 @@ import Layout from "../components/Layout";
 const Home = () => {
   return (
     <Layout>
-      <div className="grid lg:grid-cols-12 mg:grid-rows-1 md:grid-cols-6 h-1/2">
+      <div className="grid lg:grid-cols-12 md:grid-rows-1 md:grid-cols-6">
         <div className="flex items-center justify-center row-start-2 bg-black lg:col-span-5 md:col-span-4 md:row-start-1">
           <Zoom>
             <div className="py-10 space-y-10 text-white md:py-0 w-80 md:w-96">
-              <h1 className="text-3xl font-medium tracking-widest uppercase md:text-4xl">
-                create and share your photo stories.
-              </h1>
+              <h1 className="heading">create and share your photo stories.</h1>
 
               <p className="font-light">
                 Photosnap is a platform for photographers and visual
@@ -30,7 +28,7 @@ const Home = () => {
                 </Link>
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
-                  <g fill="none" fill-rule="evenodd" stroke="#fff">
+                  <g fill="none" fillRule="evenodd" stroke="#fff">
                     <path d="M0 7h41.864M35.428 1l6 6-6 6" />
                   </g>
                 </svg>
@@ -44,9 +42,72 @@ const Home = () => {
         </Fade>
       </div>
 
-      <div className="grid grid-cols-12">
-        <div className="col-span-7 bg-yellow-500">1</div>
-        <div className="col-span-5 bg-purple-500">2</div>
+      <div className="grid lg:grid-cols-12 md:grid-rows-1 md:grid-cols-6">
+        <Fade left>
+          <div className="bg-cover h-294 md:h-600 lg:col-span-7 md:col-span-2 bg-stories-sm lg:bg-stories md:bg-stories-md" />
+        </Fade>
+
+        <div className="flex items-center justify-center bg-white lg:col-span-5 md:col-span-4">
+          <Zoom>
+            <div className="py-10 space-y-10 text-black md:py-0 w-80 md:w-96">
+              <h1 className="heading w-72">Beautiful stories every time</h1>
+
+              <p className="font-light">
+                We provide design templates to ensure your stories look
+                terrific. Easily add photos, text, embed maps and media from
+                other networks. Then share your story with everyone.
+              </p>
+
+              <div className="flex items-center space-x-5 cursor-pointer">
+                <Link href="/">
+                  <a className="tracking-widest uppercase hover:underline">
+                    View the stories
+                  </a>
+                </Link>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
+                  <g fill="none" fillRule="evenodd" stroke="#000">
+                    <path d="M0 7h41.864M35.428 1l6 6-6 6" />
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </Zoom>
+        </div>
+      </div>
+
+      <div className="grid lg:grid-cols-12 md:grid-rows-1 md:grid-cols-6">
+        <div className="flex items-center justify-center row-start-2 bg-white lg:col-span-5 md:col-span-4 md:row-start-1">
+          <Zoom>
+            <div className="py-10 space-y-10 text-black md:py-0 w-80 md:w-96">
+              <h1 className="heading ">Designed for everyone</h1>
+
+              <p className="font-light">
+                Photosnap can help you create stories that resonate with your
+                audience. Our tool is designed for photographers of all levels,
+                brands, businesses you name it.
+              </p>
+
+              <div className="flex items-center space-x-5 cursor-pointer">
+                <Link href="/">
+                  <a className="tracking-widest uppercase hover:underline">
+                    View the stories
+                  </a>
+                </Link>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
+                  <g fill="none" fillRule="evenodd" stroke="#000">
+                    <path d="M0 7h41.864M35.428 1l6 6-6 6" />
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </Zoom>
+        </div>
+
+        <Fade left>
+          <div className="bg-cover h-294 md:h-600 lg:col-span-7 md:col-span-2 bg-designed-sm lg:bg-designed md:bg-designed-md" />
+        </Fade>
       </div>
     </Layout>
   );
