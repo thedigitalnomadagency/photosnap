9e6faa0b430338da2bce6f107814ca426a18f470
thedigitalnomadagency <delasimensa@gmail.com>
2021-03-11 16:01:37 +0000

    updates

--- components/StoryCard.jsx
+++ components/StoryCard.jsx
@@ -5,7 +5,7 @@ const StoryCard = ({ images, children, left }) => {
   return (
     <Fade left={left}>
       <div className="stories-wrapper">
-        <div className={`stories ${images}`}>
+        <div className={`stories md:bg-mountains bg-mountains-sm`}>
           <div className="relative flex items-end w-full h-full p-5 bg-black md:p-10 bg-opacity-30">
             {children}
           </div>
